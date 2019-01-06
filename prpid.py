df039b5d7c0c7792560edd1ca2829782826ca200
softozhik <softozhik@live.ru>
2019-01-06 22:14:43 +0300

    1. убрал запрос запуска программы - теперь запускается программа, указанная в качестве аргумента 2. выводится pid объекта запускаемого процесса

--- prpid.py
+++ prpid.py
@@ -10,10 +10,14 @@ import subprocess  # думаю, что нужен будет subprocess.Popnen
 import psutil
 
 
-# arg = sys.argv # получение аргументов из командной строки
-arg = str(input())  # пока пусть будет запрос на ввод имени программы
-# print (arg) # чисто для теста смотрим, что там в этом arg
-subprocess.Popen(arg)
+arg = sys.argv.pop() # получение аргументов из командной строки
+#arg = str(input())  # пока пусть будет запрос на ввод имени программы
+#print (arg) # чисто для теста смотрим, что там в этом arg
+launch = subprocess.Popen(arg)
+print(launch.pid)
+'''
 for proc in psutil.process_iter():
     if proc.name() == arg:
         print (proc.pid, proc.name())
+'''
+exit()
