b32a63b2bb4ac80a7180e1da5bc64e9a07fe2857
softozhik <softozhik@live.ru>
2019-01-09 14:08:19 +0300

    удалены лишние строки

--- prpid.py
+++ prpid.py
@@ -6,18 +6,9 @@
 # coding=utf8
 
 import sys  # будем использовать sys.argv
-import subprocess  # думаю, что нужен будет subprocess.Popnen
-import psutil
-
+import subprocess  # думаю, что нужен будет Popnen()
 
 arg = sys.argv.pop() # получение аргументов из командной строки
-#arg = str(input())  # пока пусть будет запрос на ввод имени программы
-#print (arg) # чисто для теста смотрим, что там в этом arg
-launch = subprocess.Popen(arg)
+launch = subprocess.Popen(arg, shell=True)
 print(launch.pid)
-'''
-for proc in psutil.process_iter():
-    if proc.name() == arg:
-        print (proc.pid, proc.name())
-'''
-exit()
+#exit()
