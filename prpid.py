32bb0f68a79a534bab4cab67bf71933ce3777f6d
softozhik <softozhik@live.ru>
2019-01-26 15:11:29 +0300

    fixed#14

--- prpid.py
+++ prpid.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
-"""
-назначение:
-запуск процесса, указанного в качестве аргумента
-вывод id запускаемого процесса
-"""
 # coding=utf8
 
-import sys  # будем использовать sys.argv
-import subprocess  # думаю, что нужен будет Popnen()
-import time
+import sys
+import subprocess
 
-args = sys.argv[1:] # получение аргументов из командной строки
-#arg.pop(0)
-#print ((a for a in arg))
+args = sys.argv[1:]
 
 if args:
     try:
         launch = subprocess.Popen(args)
-        print ('PID:', launch.pid)
+        print('PID:', launch.pid)
         try:
             launch.wait()
-            print ('Return code:', launch.returncode)
+            print('Return code:', launch.returncode)
         except KeyboardInterrupt:
             launch.kill()
-            print ('\nReturn code:', launch.returncode)
+            print('\nReturn code:', launch.returncode)
     except FileNotFoundError:
-        print ('o-ops! file not found...')
+        print('o-ops! file not found...')
     except PermissionError:
-        print ('o-ops! permission denied...')
-
-
+        print('o-ops! permission denied...')
 else:
-    print ('o-ops! no arguments...')
-#exit()
+    print('o-ops! no arguments...')
